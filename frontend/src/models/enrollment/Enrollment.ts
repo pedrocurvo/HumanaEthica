6fd3c75e8546d65e9f2c565c8f7e8d05698e4118
Joana Pimenta <joanapimenta@tecnico.ulisboa.pt>
2024-03-20 18:07:25 +0000

    feat: add volunteer id attribute to enrollment.ts

--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -1,15 +1,19 @@
 import { ISOtoString } from '@/services/ConvertDateService';
+import Volunteer from '@/models/volunteer/Volunteer';
 
 export default class Enrollment {
   id: number | null = null;
   motivation!: string;
   enrollmentDateTime!: string;
+  volunteerId!: number;
 
   constructor(jsonObj?: Enrollment) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.motivation = jsonObj.motivation;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
+      this.volunteerId = jsonObj.volunteerId;
     }
+
   }
 }
