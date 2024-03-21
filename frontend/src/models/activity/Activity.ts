23e8a5e1d201bf172b2c4df09ddf636544a6a3cd
Rodrigo Laia <rodrigo.laia@tecnico.ulisboa.pt>
2024-03-21 19:26:01 +0000

    feat: add numberOfEnrollments in activity.ts

--- frontend/src/models/activity/Activity.ts
+++ frontend/src/models/activity/Activity.ts
@@ -18,6 +18,7 @@ export default class Activity {
   formattedEndingDate!: string;
   applicationDeadline!: string;
   formattedApplicationDeadline!: string;
+  numberOfEnrollments: number | null = null;
 
   constructor(jsonObj?: Activity) {
     if (jsonObj) {
@@ -25,6 +26,7 @@ export default class Activity {
       this.name = jsonObj.name;
       this.region = jsonObj.region;
       this.participantsNumberLimit = jsonObj.participantsNumberLimit;
+      this.numberOfEnrollments = jsonObj.numberOfEnrollments;
       this.themes = jsonObj.themes.map((themes: Theme) => {
         return new Theme(themes);
       });
