1fbd48aefe21a41de72a1df940e73f593ccea6af
Rodrigo Laia <rodrigo.laia@tecnico.ulisboa.pt>
2024-03-21 19:45:59 +0000

    fix: add volunteerId and activityId in enrollment.ts

--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -2,12 +2,16 @@ import { ISOtoString } from '@/services/ConvertDateService';
 
 export default class Enrollment {
   id: number | null = null;
+  volunteerId: number | null = null;
+  activityId: number | null = null;
   motivation!: string;
   enrollmentDateTime!: string;
 
   constructor(jsonObj?: Enrollment) {
     if (jsonObj) {
       this.id = jsonObj.id;
+      this.volunteerId = jsonObj.volunteerId;
+      this.activityId = jsonObj.activityId;
       this.motivation = jsonObj.motivation;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
     }
