562586c1ea8cb85d9f4c4ba19fd7b8d951391f01
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-03-25 19:53:53 +0000

    fix: pipeline errors

--- frontend/tests/e2e/specs/activity/activity.js
+++ frontend/tests/e2e/specs/activity/activity.js
@@ -63,7 +63,7 @@ describe('Activity', () => {
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
       .eq(0).children().eq(2).should('contain', NUMBER)
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-      .eq(0).children().eq(5).should('contain', DESCRIPTION);
+      .eq(0).children().eq(4).should('contain', DESCRIPTION);
     cy.logout();
 
     cy.demoVolunteerLogin();
@@ -86,7 +86,7 @@ describe('Activity', () => {
     cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
       .eq(0).children().eq(2).should('contain', NUMBER)
     cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
-      .eq(0).children().eq(5).should('contain', DESCRIPTION);
+      .eq(0).children().eq(4).should('contain', DESCRIPTION);
     cy.logout();
 
     cy.demoAdminLogin();
@@ -115,7 +115,7 @@ describe('Activity', () => {
     cy.get('[data-cy="adminActivitiesTable"] tbody tr')
       .eq(0).children().eq(3).should('contain', NUMBER)
     cy.get('[data-cy="adminActivitiesTable"] tbody tr')
-      .eq(0).children().eq(5).should('contain', DESCRIPTION);
+      .eq(0).children().eq(4).should('contain', DESCRIPTION);
     cy.logout();
   });
 });
