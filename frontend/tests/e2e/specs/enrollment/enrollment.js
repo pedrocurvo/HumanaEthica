f6d327366d182daec88cdd2f3df15eefaedfe5ca
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-03-26 16:30:36 +0000

    fix: wrong click in enrollment.js

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -42,8 +42,7 @@ describe('Enrollment', () => {
         cy.get('[data-cy="volunteerActivities"]').click();
         // check request was done
         cy.wait('@getActivities');
-        cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
-        .eq(0).children().eq(9).get('[data-cy="applyButton"]').click();
+        cy.get('[data-cy="applyButton"]').first().click();
         cy.get('[data-cy="motivationInput"]').type('Motivation Long Enough');
         cy.get('[data-cy="saveEnrollment"]').click();
         // check request was done
