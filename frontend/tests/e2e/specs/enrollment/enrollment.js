5fcacdc33b6757915b9743990bc78cfd35e021e2
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-03-26 10:37:34 +0000

    tests: e2e test to check if enrollment is created and both volunteer and member can see it Closes #184, #185, #186

---
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -0,0 +1,80 @@
+describe('Enrollment', () => {
+    beforeEach(() => {
+      cy.deleteAllButArs();
+      cy.createDemoEntities();
+      cy.createDemoEntitiesForEnrollment();
+    });
+  
+    afterEach(() => {
+      cy.deleteAllButArs();
+    });
+
+    it('enroll in activity', () => {
+
+        // Enter in Member Demo
+        cy.demoMemberLogin()
+        // intercept create activity request and inject date values in the request body
+        cy.intercept('POST', '/enrollments', (req) => {
+            req.body = {
+            };
+        }).as('register');
+        // intercept get institutions
+        cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        cy.intercept('GET', '/themes/availableThemes').as('availableTeams')
+        cy.get('[data-cy="institution"]').click();
+
+        cy.get('[data-cy="activities"]').click();
+        cy.wait('@getInstitutions');
+
+        // Check if there are 3 activities
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .should('have.length', 3);
+        // Check if the first activity has 0 applications
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .eq(0).children().eq(3).should('contain', 0);
+        cy.logout();
+
+        // Enter in Volunteer Demo
+        cy.demoVolunteerLogin()
+        // intercept get activities request
+        cy.intercept('GET', '/activities').as('getActivities');
+        // go to volunteer activities view
+        cy.get('[data-cy="volunteerActivities"]').click();
+        // check request was done
+        cy.wait('@getActivities');
+        cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+        .eq(0).children().get('[data-cy"applyButton"]').click();
+        cy.get('[data-cy="motivationInput"]').type('Motivation Long Enough');
+        cy.get('[data-cy="saveEnrollment"]').click();
+        // check request was done
+        cy.wait('@register')
+        cy.logout();
+
+        // Enter in Member Demo
+        cy.demoMemberLogin()
+        // intercept get institutions
+        cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        cy.intercept('GET', '/themes/availableThemes').as('availableTeams')
+        cy.get('[data-cy="institution"]').click();
+
+        cy.get('[data-cy="activities"]').click();
+        cy.wait('@getInstitutions');
+
+        // Check if there are 3 activities
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .should('have.length', 3);
+        // Check if the first activity has 0 applications
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .eq(0).children().eq(3).should('contain', 1);
+        // Check motivation
+        cy.get('[data-cy="showEnrollments"]').click();
+        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').
+        should('have.length', 1);
+        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+        .eq(0).children().eq(0).should('contain', 'Motivation Long Enough');
+        cy.logout();
+
+        
+
+    });
+});
